260aff5b7df872dd543f46272d7026e088859ecd
Johannes Damp <jodamp@uni-mainz.de>
2023-05-23 11:25:48 +0200

    Add configuration of key pair

--- main.tf
+++ main.tf
@@ -13,10 +13,17 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+#Resource to Create Key Pair
+resource "aws_key_pair" "demo_key_pair" {
+  key_name   = var.key_pair_name
+  public_key = var.public_key
+}
+
 resource "aws_instance" "webserver" {
-  ami           = "ami-04e601abe3e1a910f"
-  instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  ami                    = "ami-04e601abe3e1a910f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  key_name               = aws_key_pair.demo_key_pair.key_name
 
   tags = {
     Name = var.instance_name
--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
 variable "instance_name" {
-    description = "Value of the Name tag for the EC2 instance"
-    type = string
-    default = "ExampleAppServerInstance"
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+
+variable "key_pair_name" {
+  type    = string
+  default = "demo-key-pair"
+}
+variable "public_key" {
+  description = "Public Key for ssh access"
+  type        = string
 }
