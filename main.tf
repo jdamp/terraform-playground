237b7fa51886e946b4ee25cfb1c965380a80ecb6
Johannes Damp <jodamp@uni-mainz.de>
2023-05-23 10:53:16 +0200

    Add a security group to allow inbound ssh & http

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "webserver" {
   ami           = "ami-04e601abe3e1a910f"
   instance_type = "t2.micro"
-
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
 
   tags = {
     Name = var.instance_name
